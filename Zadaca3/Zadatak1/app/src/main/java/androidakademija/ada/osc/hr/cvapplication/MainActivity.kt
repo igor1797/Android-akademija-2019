e3c27ddc4919a75356742927036cc769087afd7f
igor1797 <kuridza.igor@gmail.com>
2019-07-01 17:15:51 +0200

    initial commit

---
+++ Zadaca3/Zadatak1/app/src/main/java/androidakademija/ada/osc/hr/cvapplication/MainActivity.kt
@@ -0,0 +1,12 @@
+package androidakademija.ada.osc.hr.cvapplication
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
